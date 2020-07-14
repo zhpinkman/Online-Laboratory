26792f59af2c216a38ceccc498901cc07f152791
zhivar <zhivarsourati@gmail.com>
2020-07-14 20:21:56 +0430

    Add temp lab and lab tests

--- src/main/java/com/example/demo/domain/handlers/LabHandler.java
+++ src/main/java/com/example/demo/domain/handlers/LabHandler.java
@@ -13,11 +13,32 @@ public class LabHandler {
 
     public LabHandler() {
         Lab lab1 = new Lab("lab", new Address(100, 200, "baqalia"));
+
+//        lab repository
+
         LabRepository labRepository1 = new LabRepository("repo", "LDfaLk");
         lab1.setLabRepositories(labRepository1);
+
+//        insuranceCompany
+
         List<InsuranceCompany> insuranceCompanies = new ArrayList<InsuranceCompany>(Arrays.asList(new InsuranceCompany("bime", 80)));
         lab1.setSupportedInsurances(insuranceCompanies);
-        List<LabTest> labTestList = new ArrayList<>();
+
+//        lab tests
+
+        TestDesc testDesc1 = new TestDesc("test1", 1, false, true, "nothing", "nothing");
+        TestDesc testDesc2 = new TestDesc("test2", 2, false, true, "nothing", "nothing");
+        TestDesc testDesc3 = new TestDesc("test3", 3, false, true, "nothing", "nothing");
+        TestDesc testDesc4 = new TestDesc("test4", 2, false, true, "nothing", "nothing");
+
+        LabTest labTest1 = new LabTest(2000, testDesc1);
+        LabTest labTest2 = new LabTest(3000, testDesc2);
+        LabTest labTest3 = new LabTest(4000, testDesc3);
+        LabTest labTest4 = new LabTest(4000, testDesc4);
+
+        List<LabTest> labTestList = new ArrayList<LabTest>(Arrays.asList(labTest1, labTest2, labTest3, labTest4));
+
+        lab1.setLabTests(labTestList);
 
     }
 
