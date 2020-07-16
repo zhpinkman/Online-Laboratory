22fafe1b34ca7ee117a75cbafb12a560efc76d61
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-07-16 13:08:37 +0430

    Add test function to draw sequence diagram

--- src/main/java/com/example/demo/OnlineLabApplication.java
+++ src/main/java/com/example/demo/OnlineLabApplication.java
@@ -1,13 +1,51 @@
 package com.example.demo;
 
+import com.example.demo.controllers.MzLabController;
+import com.example.demo.domain.handlers.MzLab;
+import com.example.demo.domain.lab.TestDesc;
+import com.example.demo.domain.user.Prescription;
+import com.example.demo.domain.utility.Address;
+import com.example.demo.domain.utility.FullTestInfo;
+import com.example.demo.domain.utility.Receipt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class OnlineLabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineLabApplication.class, args);
+		runTestCommands();
+	}
+
+	private static void runTestCommands(){
+		try {
+			MzLab mzLab = MzLab.getInstance();
+			List<TestDesc> tests = mzLab.getAllTests();
+			List<Prescription> presc = mzLab.getReviewedPrescriptions();
+
+			List<String> selectedTests = new ArrayList<>();
+			selectedTests.add(tests.get(0).getTestName());  ////
+
+			mzLab.setPatientTests(selectedTests);
+			List<Address> addresses = mzLab.getPatientAddresses();
+			mzLab.setPatientTestAddress(addresses.get(0));
+			mzLab.attachPrescriptionToTest(presc.get(0).getId());
+			List<FullTestInfo> fullTestInfoList = mzLab.verifyPatientTestRequest();
+			mzLab.setSelectedLabForTests(fullTestInfoList.get(0).getLabName());
+			List<Date> dates = mzLab.confirmTestInfo();
+			Receipt receipt = mzLab.selectTimeForTest(dates.get(0));
+			mzLab.confirmPaymentReceipt();
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+
 	}
 
 }
